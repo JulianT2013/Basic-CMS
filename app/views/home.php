bb51f7f404d4676aaddcdb685eb66c1f9a559ace
JulianT2013 <jh_tapping@yahoo.com.au>
2018-01-09 16:07:37 +1100

    Created views.

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,15 @@
-Home
+<?php require VIEW_ROOT . '/templates/header.php'; ?>
+
+	<?php if ( empty( $pages ) ): ?>
+		<p>Yes, we have no pages. We have no pages today.</p>
+
+	<?php else: ?>
+		<ul>
+			<?php foreach ($pages as $page): ?>
+				<li><a href="<?php echo BASE_URL; ?>/page.php?page=<?php echo $page['slug']; ?>"><?php echo $page['label']; ?></a></li>
+				
+			<?php endforeach; ?>
+
+	<?php endif; ?>
+
+<?php require VIEW_ROOT . '/templates/footer.php'; ?>
---
+++ app/views/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>My CMS</title>
+
+		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>/css/app.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<h1>Dude, Where's My CMS?</h1>
